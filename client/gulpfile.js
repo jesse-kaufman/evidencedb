e9e7641a02e84fee0eee4372c7001ab693344004
Jesse Kaufman <jesse@jessekaufman.com>
2024-03-29 07:43:43 -0500

    Update formatting

--- client/gulpfile.js
+++ client/gulpfile.js
@@ -1,13 +1,14 @@
-const gulp = require('gulp');
-const sass = require('gulp-sass')(require('sass'));
+const gulp = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
 
-gulp.task('sass', function (cb) {
-    gulp.src('src/styles/src/*.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('src/styles'));
-    cb();
+gulp.task("sass", function (cb) {
+  gulp
+    .src("src/styles/src/*.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(gulp.dest("src/styles"));
+  cb();
 });
 
-gulp.task('default', function () {
-    gulp.watch('src/styles/src/*', gulp.parallel('sass'));
+gulp.task("default", function () {
+  gulp.watch("src/styles/src/*", gulp.parallel("sass"));
 });
