6d7c8d3c7b1ab0c433c5da9622a9cf849f616048
jesse-kaufman <jesse@jessekaufman.com>
2024-05-22 12:21:08 -0500

    Fix substring() calls

--- node-only/utils/helpers.js
+++ node-only/utils/helpers.js
@@ -1,6 +1,6 @@
 exports.formatPhone = (number) =>
   number.substring(0, 3) +
   "-" +
-  number.substring(3, 3) +
+  number.substring(3, 6) +
   "-" +
-  number.substring(-4);
+  number.substring(6);
