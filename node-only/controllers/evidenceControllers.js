ef316cdf83208c8c2128c61e98e2220a776d07b3
jesse-kaufman <jesse@jessekaufman.com>
2024-05-22 12:20:42 -0500

    Add formattedDate to projection

--- node-only/controllers/evidenceControllers.js
+++ node-only/controllers/evidenceControllers.js
@@ -8,7 +8,28 @@ exports.printEvidence = async function (req, res) {
   let numbers = null;
   let query = getQuery(req);
   let stats = await getStats(req.query.include, query);
-  let evidenceItems = await EvidenceItem.find(query).sort({ date_sent: 1 });
+  let evidenceItems = await EvidenceItem.aggregate([{ $match: query }])
+    .sort({
+      date_sent: 1,
+    })
+    .project({
+      type: 1,
+      date_sent: 1,
+      from: 1,
+      to: 1,
+      direction: 1,
+      victim: 1,
+      body: 1,
+      body_html: 1,
+      subject: 1,
+      duration: 1,
+      filename: 1,
+      attachments: 1,
+      title: 1,
+      formattedDate: {
+        $dateToString: { format: "%b %d, %Y", date: "$date_sent" },
+      },
+    });
   let dates = await getDates(req.query.include);
 
   if (!req.query.include) {
