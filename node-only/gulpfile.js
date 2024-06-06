c38991c031c3897647e4b01d265f384126c3c81f
jesse-kaufman <jesse@jessekaufman.com>
2024-06-05 19:50:56 -0500

    Use command line tools for build

--- node-only/gulpfile.js
+++ node-only/gulpfile.js
@@ -7,23 +7,40 @@ import gitGuppy from "git-guppy";
 import gulpFilter from "gulp-filter";
 import jshint from "gulp-jshint";
 import stylish from "jshint-stylish";
-import ts from "gulp-typescript";
+import exec from "gulp-exec";
 
 const sass = gulpSass(dartSass);
 const guppy = gitGuppy(gulp);
 
-task("sass", function (cb) {
-  src("src/styles/*.scss")
-    .pipe(sass().on("error", sass.logError))
-    .pipe(dest("public/css"));
-  cb();
+const execOptions = {
+  continueOnError: false, // default = false, true means don't emit error event
+  pipeStdout: false, // default = false, true means stdout is written to file.contents
+};
+const execReportOptions = {
+  err: true, // default = true, false means don't write err
+  stderr: true, // default = true, false means don't write stderr
+  stdout: true, // default = true, false means don't write stdout
+};
+
+// task("sass", function (cb) {
+//   src("src/styles/*.scss")
+//     .pipe(sass().on("error", sass.logError))
+//     .pipe(dest("public/css"));
+//   cb();
+// });
+
+task("sass", function () {
+  return gulp
+    .src(".")
+    .pipe(exec((file) => `sass ${file.path}`, execOptions))
+    .pipe(exec.reporter(execReportOptions));
 });
 
-task("ts", function (cb) {
-  let tsProject = ts.createProject("tsconfig.json");
-
-  src("src/js/*.ts").pipe(tsProject()).pipe(dest("public/js"));
-  cb();
+task("ts", function () {
+  return gulp
+    .src(".")
+    .pipe(exec((file) => `tsc -p ${file.path}`, execOptions))
+    .pipe(exec.reporter(execReportOptions));
 });
 
 task("default", function () {
