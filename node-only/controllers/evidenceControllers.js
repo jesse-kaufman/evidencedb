051906ad4eee3b3fe07d9e2a3874f60a2f4cf7f6
jesse-kaufman <jesse@jessekaufman.com>
2024-06-08 13:06:36 -0500

    Move to separate getEvidenceItems function

--- node-only/controllers/evidenceControllers.js
+++ node-only/controllers/evidenceControllers.js
@@ -5,7 +5,7 @@
  * @author Jesse Kaufman <jesse@jessekaufman.com>
  */
 
-import EvidenceItem from "../models/evidenceItemModel.js";
+import { getEvidenceItems } from "../models/evidenceItemModel.js";
 import { getQuery } from "../utils/queryUtils.js";
 import { getStats, getDates, getNumbers } from "../utils/evidenceUtils.js";
 import {
@@ -26,55 +26,14 @@ export async function printEvidence(req, res) {
 
   // Build the query from the request object
   let query = getQuery(req);
-  let dateFilter = {};
 
   // Get stats for the query to display on frontend
   let stats = await getStats(req.query.include, query);
 
-  // Filter based on date
-  if (req.query.date_sent_date) {
-    dateFilter = {
-      dateSentDate: req.query.date_sent_date,
-    };
-  }
-
   // Get matching evidence items for the query
-  let evidenceItems = await EvidenceItem.aggregate([{ $match: query }])
-    .sort({
-      date_sent: 1,
-    })
-    .project({
-      type: 1,
-      date_sent: 1,
-      from: 1,
-      to: 1,
-      direction: 1,
-      victim: 1,
-      body: 1,
-      body_html: 1,
-      subject: 1,
-      duration: 1,
-      filename: 1,
-      attachments: 1,
-      screenshots: 1,
-      title: 1,
-      formattedDate: {
-        $dateToString: {
-          format: "%b %d, %Y",
-          date: "$date_sent",
-          timezone: "America/Chicago",
-        },
-      },
-      dateSentDate: {
-        $dateToString: {
-          format: "%Y-%m-%d",
-          date: "$date_sent",
-          timezone: "America/Chicago",
-        },
-      },
-    })
-    .match(dateFilter);
+  let evidenceItems = await getEvidenceItems(query, req.query.date_sent_date);
 
+  // Get dates for dropdown items
   let dates = await getDates(req.query.include);
 
   // Only get phone number list if req.query.include is unset
--- node-only/models/evidenceItemModel.js
+++ node-only/models/evidenceItemModel.js
@@ -48,4 +48,50 @@ const EvidenceItemSchema = new mongoose.Schema({
 
 const EvidenceItemModel = new mongoose.model("Item", EvidenceItemSchema);
 
+export const getEvidenceItems = async function (query, dateSentDate = "") {
+  var dateFilter = {};
+  // Filter based on date
+  if (dateSentDate) {
+    dateFilter = {
+      dateSentDate: dateSentDate,
+    };
+  }
+
+  // Get matching evidence items for the query
+  return await EvidenceItemModel.aggregate([{ $match: query }])
+    .sort({
+      date_sent: 1,
+    })
+    .project({
+      type: 1,
+      date_sent: 1,
+      from: 1,
+      to: 1,
+      direction: 1,
+      victim: 1,
+      body: 1,
+      body_html: 1,
+      subject: 1,
+      duration: 1,
+      filename: 1,
+      attachments: 1,
+      screenshots: 1,
+      title: 1,
+      formattedDate: {
+        $dateToString: {
+          format: "%b %d, %Y",
+          date: "$date_sent",
+          timezone: "America/Chicago",
+        },
+      },
+      dateSentDate: {
+        $dateToString: {
+          format: "%Y-%m-%d",
+          date: "$date_sent",
+          timezone: "America/Chicago",
+        },
+      },
+    })
+    .match(dateFilter);
+};
 export default EvidenceItemModel;
