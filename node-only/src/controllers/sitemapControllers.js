b19cc4596553dce7579cad209d91a86175e6c72f
jesse-kaufman <jesse@jessekaufman.com>
2024-06-11 14:50:25 -0500

    Clean up code

--- node-only/src/controllers/sitemapControllers.js
+++ node-only/src/controllers/sitemapControllers.js
@@ -1,5 +1,4 @@
 import { getEvidenceItems } from "../models/evidenceItemModel.js";
-import { getQuery } from "../utils/queryUtils.js";
 export const printSitemap = async (req, res) => {
   // Get matching evidence items for the query
   let evidenceItems = await getEvidenceItems({});
