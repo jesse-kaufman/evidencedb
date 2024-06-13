5eacddddbfe4e38ae8a2c9bd5cd5b4fdde59f14e
jesse-kaufman <jesse@jessekaufman.com>
2024-06-13 16:04:21 -0500

    Add comments

--- node-only/src/routes/staticRoutes.js
+++ node-only/src/routes/staticRoutes.js
@@ -1,21 +1,26 @@
+// Static routes
 import express from "express";
 const router = express.Router();
 
+// Default options
 const staticOptions = {
   maxAge: "2y",
   etag: false,
 };
 
-// Static routes
+// Route for CSS/JS files
 router.use("/public", express.static("build/public", staticOptions));
+// Route for images
 router.use(
   "/public/images",
   express.static("src/public/images", staticOptions)
 );
+// Route for Lightbox2
 router.use(
   "/libs/lightbox2",
   express.static("node_modules/lightbox2/dist", staticOptions)
 );
+// Route for jQuery
 router.use(
   "/libs/jquery/jquery.min.js",
   express.static("node_modules/jquery/dist/jquery.min.js", staticOptions)
