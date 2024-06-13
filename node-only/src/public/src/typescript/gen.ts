ea9bb244e208cc5b552e097b58cc054a63a231f1
jesse-kaufman <jesse@jessekaufman.com>
2024-06-13 12:21:08 -0500

    Fix clicking on transcript title

--- node-only/src/public/src/typescript/gen.ts
+++ node-only/src/public/src/typescript/gen.ts
@@ -2,7 +2,7 @@
  * Sets up the event listeners for the transcript links.
  */
 const setupTranscript = (): void => {
-  $("body.list .transcript h1").on("click", (e) => {
+  $("body.list .transcript h2").on("click", (e) => {
     if ($(e.currentTarget).parent().hasClass("show")) {
       // Hide transcript
       $(e.currentTarget).parent().removeClass("show");
