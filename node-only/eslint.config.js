e2c1971e1c3c92686580e8889fd6ab8631f83bce
jesse-kaufman <jesse@jessekaufman.com>
2024-06-14 21:42:40 -0500

    Add more rules

--- node-only/eslint.config.js
+++ node-only/eslint.config.js
@@ -2,11 +2,28 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 
-export default [
+const config = [
   { ignores: ["**/node_modules/*", "**/public/js/*"] },
   {
     languageOptions: { globals: { ...globals.browser, ...globals.node } },
   },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  {
+    rules: {
+      "no-await-in-loop": "error",
+      "no-duplicate-imports": "error",
+      "no-inner-declarations": "error",
+      "no-self-compare": "error",
+      "no-template-curly-in-string": "error",
+      "no-unmodified-loop-condition": "error",
+      "no-unreachable-loop": "error",
+      "no-use-before-define": "warn",
+      "require-atomic-updates": "warn",
+      "capitalized-comments": ["warn", "always"],
+      camelcase: "warn",
+    },
+  },
 ];
+
+export default config;
