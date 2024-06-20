37af5f63ecfefe040f3bd6d189b66f819ef15261
jesse-kaufman <jesse@jessekaufman.com>
2024-06-19 23:02:51 -0500

    Add route for /type and reorder

--- node-only/src/routes/evidenceRoutes.js
+++ node-only/src/routes/evidenceRoutes.js
@@ -4,8 +4,14 @@ import express from "express";
 import sitemapController from "../controllers/sitemapControllers.js";
 
 const router = new express.Router();
-router.get("/", evidenceController.render);
-router.get("/evidence-item/:id", evidenceController.render);
+
+// Sitemap route
 router.get("/sitemap.xml", sitemapController.render);
+
+// Main routes
+router.get("/", evidenceController.render);
+router.get("/evidence-item/:id", evidenceController.redirect);
+router.get("/:type/evidence-item/:id", evidenceController.render);
+router.get("/:type", evidenceController.render);
 
 export default router;
