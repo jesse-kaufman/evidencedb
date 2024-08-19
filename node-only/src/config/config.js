8da8c8e28f4e0d1c72da390cfdbf39f529c1299b
jesse-kaufman <jesse@jessekaufman.com>
2024-08-19 17:25:59 -0500

    Clean up setting base URL config item

--- node-only/src/config/config.js
+++ node-only/src/config/config.js
@@ -1,12 +1,11 @@
 // Environment variables
 
-
-process.env.BASE_URL = process.env.DEV_BASE_URL;
-
-if (process.env.NODE_ENV === "production") {
-  process.env.BASE_URL = process.env.PROD_BASE_URL;
-}
+const baseUrl =
+  process.env.NODE_ENV === "production"
+    ? process.env.BASE_URL
+    : process.env.DEV_BASE_URL;
 
 export default {
+  baseUrl,
   mongoUrl: process.env.MONGO_URL,
 };
