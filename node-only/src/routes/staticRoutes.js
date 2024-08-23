75e44f2b0c05949477b4280c2702365f20c20b9a
jesse-kaufman <jesse@jessekaufman.com>
2024-08-23 06:21:13 -0500

    Format with prettier

--- node-only/src/routes/staticRoutes.js
+++ node-only/src/routes/staticRoutes.js
@@ -1,31 +1,32 @@
 // Static routes
-import express from "express";
-const router = new express.Router();
+import express from 'express'
+const router = new express.Router()
 
 // Default options
 const staticOptions = {
-  maxAge: "2y",
+  maxAge: '2y',
   etag: false,
-};
+}
 
 // Route for CSS/JS files
-router.use("/public", express.static("build/public", staticOptions));
+router.use('/public', express.static('build/public', staticOptions))
 // Route for images
-router.use(
-  "/public/images",
-  express.static("src/public/images", staticOptions)
-);
+router.use('/public/images', express.static('src/public/images', staticOptions))
 // Route for Lightbox2
 router.use(
-  "/libs/lightbox2",
-  express.static("node_modules/lightbox2/dist", staticOptions)
-);
+  '/libs/lightbox2',
+  express.static('node_modules/lightbox2/dist', staticOptions),
+)
 // Route for jQuery
 router.use(
-  "/libs/jquery/jquery.min.js",
-  express.static("node_modules/jquery/dist/jquery.min.js", staticOptions)
-);
+  '/libs/jquery/jquery.min.js',
+  express.static('node_modules/jquery/dist/jquery.min.js', staticOptions),
+)
 // Route for favicon.ico
+router.use(
+  '/favicon.ico',
+  express.static('src/public/images/favicon.ico', staticOptions),
+)
 router.use(
   '/robots.txt',
   express.static('src/public/robots.txt', staticOptions),
@@ -35,4 +36,4 @@ router.use(
   express.static('src/public/site.webmanifest', staticOptions),
 )
 
-export default router;
+export default router
