ec28c08d4bc3aa50742e47dc30d7acd1c7fbbc1d
jesse-kaufman <jesse@jessekaufman.com>
2025-03-20 09:00:26 -0500

    fix: use config for base URL

--- node-only/src/controllers/sitemapControllers.js
+++ node-only/src/controllers/sitemapControllers.js
@@ -1,9 +1,14 @@
-import { getEvidenceItems } from "../models/evidenceItemModel.js";
+import { getEvidenceItems } from '../models/evidenceItemModel.js'
+import config from '../config/config.js'
 const render = async (req, res) => {
   // Get matching evidence items for the query
-  const evidenceItems = await getEvidenceItems({});
+  const evidenceItems = await getEvidenceItems({})
 
-  await res.type("xml").render("sitemap_xml", { evidenceItems: evidenceItems });
-};
+  await res.type('xml').render('sitemap_xml', {
+    evidenceItems: evidenceItems,
+    baseUrl: config.baseUrl,
+    cdnUrl: process.env.CDN,
+  })
+}
 
-export default { render };
+export default { render }
